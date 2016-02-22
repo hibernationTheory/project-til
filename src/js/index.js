e40abb379f19b1f5abc867824142dfaf4f9866e9
Engin Arslan <hibernationtheory@gmail.com>
2016-02-21 21:01:38 -0500

    change the history to hash history

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {IndexRoute, Route, Router, browserHistory} from 'react-router'
+import {IndexRoute, Route, Router, hashHistory} from 'react-router'
 
 import ContainerPage from './components/ContainerPage'
 import MainPage from './components/MainPage'
@@ -10,16 +10,16 @@ var css = require("!style!css!sass!../scss/styles.scss");
 
 
 ReactDOM.render(
-    <Router history={browserHistory}>
-        <Route path="/" component={ContainerPage}>
-            <IndexRoute component={MainPage} />
-            <Route path="/page/:page" component={MainPage} />
-        	<Route path="/post/:post" component={Post} />
-        	<Route path="/category/:category" component={MainPage}>
-        		<Route path=":page" component={MainPage} />
-        	</Route>
-        </Route>
-    </Router>,
+	<Router history={hashHistory}>
+		<Route path="/" component={ContainerPage}>
+			<IndexRoute component={MainPage} />
+			<Route path="/page/:page" component={MainPage} />
+			<Route path="/post/:post" component={Post} />
+			<Route path="/category/:category" component={MainPage}>
+				<Route path=":page" component={MainPage} />
+			</Route>
+		</Route>
+	</Router>,
   document.getElementById('react-container')
 );
 
